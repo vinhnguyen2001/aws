9fa0b4837746a8862d8130f7f5f8d271c76b0839
vinhnguyen2001 <vinhnguyentran2001@gmail.com>
2025-12-10 23:17:52 +0700

    fix prevent creating public bucket

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -10,7 +10,7 @@ private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
 
 # RDS configuration
 db_username         = "admin"
-db_password         = "xxxAdmin123@"
+db_password         = "Admin123!Pass"
 db_instance_class   = "db.t3.micro"
 db_allocated_storage = 20
 db_multi_az         = false  # Set to true for production
--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -14,11 +14,11 @@ resource "aws_s3_bucket_public_access_block" "aws_boostup_bucket_public_access_b
   bucket = aws_s3_bucket.aws_boostup_bucket.id
 
 
-  # Settings to block public access
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  # Settings to allow public access for website hosting
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 
